a8e8537844e8e9de0995a689b47ff79c80dd588b
divya sy <sydivyyaa@gmail.com>
2025-06-07 21:31:49 +0530

    Update WebpageTest.java

--- src/test/java/org/test/WebpageTest.java
+++ src/test/java/org/test/WebpageTest.java
@@ -1,53 +1,40 @@
 package org.test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.testng.Assert.assertTrue;
+
 public class WebpageTest {
+
     public WebDriver driver;
-  //  private static WebDriver driver;
 
     @BeforeClass
-    public void setupBrowser() {
-        io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
-       System.setProperty("webdriver.chrome.driver", System.getenv("WEBDRIVER_CHROME_DRIVER"));
-        driver = new ChromeDriver();
-        driver.get("https://codersarcade.com");
-    }
-    @BeforeTest
-    public void openBrowser() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-
-     //   WebDriverManager.chromedriver().setup(); // Automatically downloads correct ChromeDriver
-       //  driver = new ChromeDriver();
-    //  WebDriver driver = new ChromeDriver();
+    public void setupBrowser() throws InterruptedException {
+        WebDriverManager.chromedriver().setup(); // Downloads the right ChromeDriver
+        driver = new ChromeDriver(); // use class-level driver
         driver.manage().window().maximize();
-        Thread.sleep(2000);
+        Thread.sleep(2000); // only for demo; not ideal in real testing
         driver.get("https://sauravsarkar-codersarcade.github.io/DevOps-VTU-MVN/");
     }
+
     @Test
-    public void titleValidationTest(){
+    public void titleValidationTest() {
         String actualTitle = driver.getTitle();
-        System.out.println("Title found: " + driver.getTitle());
+        System.out.println("Title found: " + actualTitle);
         String expectedTitle = "Coders Arcade - CI/CD Learning";
-       // String expectedTitle = "Example Domain";
         Assert.assertEquals(actualTitle, expectedTitle, "Page title doesn't match!");
-        assertTrue(true, "Title contains 'CI/CD'");
+        assertTrue(actualTitle.contains("CI/CD"), "Title should contain 'CI/CD'");
     }
-    @AfterTest
+
+    @AfterClass
     public void closeBrowser() throws InterruptedException {
         Thread.sleep(1000);
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
-
 }
